24b4b20a87d701f907046e3f7d9f92c47fae1822
Angela <angela.doxsey@yahoo.com>
2013-06-19 09:28:06 -0400

    Added num hierarchy edit text preference

--- src/main/java/org/openhds/mobile/activity/LocationHierarchyActivity.java
+++ src/main/java/org/openhds/mobile/activity/LocationHierarchyActivity.java
@@ -10,6 +10,7 @@ import android.widget.Toast;
 
 public class LocationHierarchyActivity extends PreferenceActivity implements OnSharedPreferenceChangeListener {
 	
+    public static String NUM_HIERARCHIES = "numHierarchies";
 	public static String HIERARCHY_1 = "hierarchy1";
     public static String HIERARCHY_2 = "hierarchy2";
     public static String HIERARCHY_3 = "hierarchy3";
@@ -19,15 +20,39 @@ public class LocationHierarchyActivity extends PreferenceActivity implements OnS
      * a number is chosen, that number of text field input boxes should be supplied to
      * receive input.
      */
+    
+    /*  <EditTextPreference
+    		android:id="@+id/numHierarchies"
+            android:key="numHierarchies"
+            android:defaultValue="@string/default_numHierarchies"
+            android:title="@string/numHierarchiesTitle"
+            android:dialogTitle="@string/changeNumHierarchy" />
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.layout.location_hierarchies);
         setTitle(getString(R.string.app_name) + " > " + getString(R.string.configureHierarchies));
+        updateNumHierarchies(NUM_HIERARCHIES);
         updateHierarchy(HIERARCHY_1);
         updateHierarchy(HIERARCHY_2);
         updateHierarchy(HIERARCHY_3);
         updateHierarchy(HIERARCHY_4);
+    }
+    
+    private void updateNumHierarchies(String numHierarchies){
+    	EditTextPreference etp = (EditTextPreference) this.getPreferenceScreen().findPreference(numHierarchies);
+        
+        String s = etp.getText().trim();
+
+        if (!s.isEmpty()) {
+            etp.setText(s);
+            etp.setSummary(s);
+        } 
+        else {
+            etp.setText((String) etp.getSummary());
+            Toast.makeText(getApplicationContext(), getString(R.string.num_hierarchy_error), Toast.LENGTH_SHORT).show();
+        }
     }
     
     private void updateHierarchy(String hierarchy) {
